d90998ab0d660e158eb0668fe3621a9a0daf2b29
chakripavankumar <siriyalapavankumar@gmail.com>
2023-12-29 00:46:31 +0530

    MVC✅

---
+++ connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+async function conectToMongoDB(url) {
+    return mongoose.connect(url);
+}
+
+module.exports = connectToMongoDB;
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
-
+const{connectToMongoDB}= require('./connect')
+const urlRoute= require('./routes/url');
 const app = express();
 const PORT = 3000;
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(()=> console.log("MongoDB connected"))
+
+app.use("/url", urlRoute);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT,
     () => console.log(` port is listening on ${PORT}`));
